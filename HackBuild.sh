3b57771cb14ff3b05f224b46fb0ce61d05c6d7f6
voldmort21 <38194695+voldmort21@users.noreply.github.com>
2018-06-18 19:22:50 +0300

    Add files via upload

---
+++ HackBuild.sh
@@ -0,0 +1,99 @@
+#!/bin/bash
+"#############################################"
+echo  welcome to my script
+"##############################################"
+apt install figlet
+figlet 3xploit
+apt install sl
+sl
+echo 3xploit
+"####################"
+figlet nmap
+"#################"
+echo instalando nmap
+apt install nmap
+apt install python
+figlet python
+apt install php
+figlet php
+apt install python2
+"#################"
+echo installing python2
+"###################"
+echo 3xploit weclome to my script
+figlet red_hawk
+git clone https://github.com/Tuhinshubhra/RED_HAWK.git
+figlet credmap
+git clone https://github.com/lightos/credmap.git
+figlet sqlmap
+git clone https://github.com/sqlmapproject/sqlmap.git
+figlet recondog
+git clone https://github.com/UltimateHackers/ReconDog.git
+figlet ngrok
+git clone https://github.com/inconshreveable/ngrok.git
+apt install tor
+echo installing tor
+apt install perl
+apt install w3m
+echo installing w3m
+apt install apache2
+apt install cowsay
+
+"###########################"
+cowsay  Goodbye..
+"###########################"
+#######################################
+
+#!/bin/bash
+##########################
+echo welcome to BestBuild
+##########################
+apt install figlet
+#################
+figlet lets start
+#################
+apt install sl
+sl
+##############
+apt install cowsay
+apt install apache2
+apt install tor
+apt install perl
+apt install coreutils
+apt install termux-tools
+apt install make
+apt install nano
+apt install python
+apt install python2
+apt install nmap
+apt install wget
+apt install git
+apt install gzip
+apt install g++
+apt install clang
+apt install libncurses5-dev
+apt install git-core
+apt install gnupg
+apt install flex
+apt install bison
+apt install gperf
+apt install zip
+apt install curl
+apt install libc6-dev
+apt install ccache
+apt install automake
+apt install lzop
+apt install optipng
+apt install liblz4-tool
+apt install schedtool
+apt install squashfs-tools
+#############################
+echo it may take some minutes
+#############################
+git clone https://github.com/arter97/linaro-64.git
+git clone
+https://android.googlesource.com/platform/prebuilts/gc$
+#########################
+figlet Done
+#########################
+done
